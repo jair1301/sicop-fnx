60f5f921df7124a766d8626a3ee76f005760e2ec
enriquerojas1301@gmail.com <enriquerojas1301@gmail.com@096234b6-f144-21d4-26a6-6014722fc827>
2014-06-09 05:53:51 +0000

    listo el crud para dar de alta un usuario del sistema

---
+++ trunk/SICOP-FNX/src/java/com/sicopFnx/usuarios/dao/UsuarioDAO.java
@@ -0,0 +1,194 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sicopFnx.usuarios.dao;
+
+import com.sicopFnx.usuarios.bean.UsuarioBean;
+import com.sicopFnx.utileria.conexionMySql;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+/**
+ *
+ * @author Enrique
+ */
+public class UsuarioDAO {
+    
+public static String SqlAltaUsuario = "INSERT INTO usuario VALUES (null, ?, ?, ?, ?, ?);";
+    public static String SqlBajaUsuario = "DELETE FROM usuario WHERE idusuario=?;";
+    public static String SqlModificarUsuario = "UPDATE usuario SET nombre=?, apellido=?, sexo=?, password=?, login=? WHERE idusuario=?;";
+    public static String SqlSelectTodosUsuario = "SELECT * FROM usuario";
+
+    Connection con;
+    PreparedStatement pstm;
+    ResultSet rs;
+    
+    public UsuarioBean ConsultaUsuarioIndividual(int UsuarioId){
+    
+            UsuarioBean uno = new UsuarioBean();
+    
+        try {
+            con = conexionMySql.getConnection();
+            pstm = con.prepareStatement(SqlSelectTodosUsuario+" where idusuario=?;");
+            pstm.setInt(1, UsuarioId);
+            
+            rs=pstm.executeQuery();
+            while(rs.next()){
+            
+            uno.setIdUsuario(rs.getInt("idusuario"));
+            uno.setNombre(rs.getString("nombre"));
+            uno.setApellido(rs.getString("apellido"));
+            uno.setPassword(rs.getString("password"));
+            uno.setLogin(rs.getString("login"));
+            
+            System.out.println(uno.getIdUsuario());
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                pstm.close();
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return uno;
+    }
+
+    public boolean altaUsuario(UsuarioBean BeanUsuario) {
+
+        boolean respuesta = false;
+
+        try {
+            con = conexionMySql.getConnection();
+            pstm = con.prepareStatement(SqlAltaUsuario);
+            pstm.setString(1, BeanUsuario.getNombre());
+            pstm.setString(2, BeanUsuario.getApellido());
+            pstm.setString(4, BeanUsuario.getPassword());
+            pstm.setString(5, BeanUsuario.getLogin());
+            
+            respuesta=pstm.execute();
+            
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                pstm.close();
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return respuesta;
+    }
+    
+    public boolean bajaUsuario(int x){
+    
+    
+        boolean respuesta = false;
+
+        try {
+            con = conexionMySql.getConnection();
+            pstm = con.prepareStatement(SqlBajaUsuario);
+            pstm.setInt(1, x);
+            
+            respuesta=pstm.execute();
+            
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                pstm.close();
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return respuesta;
+    }
+    
+    public List consultaUsuario(){
+       
+        List <UsuarioBean> respuesta= new ArrayList();
+        try {
+            con = conexionMySql.getConnection();
+            pstm = con.prepareStatement(SqlSelectTodosUsuario);
+            
+            rs=pstm.executeQuery();
+            while(rs.next()){
+            
+            UsuarioBean uno = new UsuarioBean();
+            uno.setIdUsuario(rs.getInt("idusuario"));
+            uno.setNombre(rs.getString("nombre"));
+            uno.setApellido(rs.getString("apellido"));
+             uno.setPassword(rs.getString("password"));
+            uno.setLogin(rs.getString("login"));
+            
+            respuesta.add(uno);
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                pstm.close();
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return respuesta;
+    
+    
+    }
+    
+    public boolean modificarUsuario(UsuarioBean BeanUsuario){
+    
+        boolean respuesta = false;
+
+        try {
+            con = conexionMySql.getConnection();
+            pstm = con.prepareStatement(SqlModificarUsuario);
+            pstm.setString(1, BeanUsuario.getNombre());
+            pstm.setString(2, BeanUsuario.getApellido());
+            pstm.setString(4, BeanUsuario.getPassword());
+            pstm.setString(5, BeanUsuario.getLogin());
+            pstm.setInt(6, BeanUsuario.getIdUsuario());
+            respuesta=pstm.execute();
+            
+            System.out.println(pstm);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                pstm.close();
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return respuesta;
+        
+    
+    }
+    
+    
+    
+    public static void main(String[] args) {
+           }
+    
+
+}
---
+++ trunk/SICOP-FNX/web/JSP/Usuario/UsuarioConsultar.jsp
@@ -0,0 +1,81 @@
+<%-- 
+    Document   : UsuarioConsultar
+    Created on : 9/06/2014, 12:45:02 AM
+    Author     : Enrique
+--%>
+
+
+<%@page import="com.sicopFnx.usuarios.bean.UsuarioBean"%>
+<%@page import="java.util.List"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+   
+    <script >
+           alert("holamundo");
+       </script>
+
+      <%
+       String msj= (String)request.getAttribute("msj2");
+      
+       %>
+       <script >
+           alert("<%=msj%>");
+       </script>
+
+       <%
+       
+        %>
+        
+    <body>
+        <h1>Hello World!</h1>
+        <% String context= request.getContextPath();
+        List<UsuarioBean> usuariosLista=( List)request.getAttribute("usuariosLista");
+        if(usuariosLista == null){
+            %>
+         no existen usuarios
+            <%
+        }else {
+        %>
+        si existen usuarios
+        <a href="<%=context %>JSP/Usuario/UsuarioAlta.jsp">dar De alta un usuario</a>
+        
+        
+        <table><tr>
+            <td>id:</td>
+            <td>nombre:</td>
+            <td>apellido:</td>
+            <td>password:</td>
+            <td>login:</td>
+            <td>Eliminar:</td>
+            
+            </tr>
+            <%
+            for(UsuarioBean bean:usuariosLista){
+            %>
+            <tr>
+                <td><%=bean.getIdUsuario() %></td>
+                <td><%=bean.getNombre() %></td>
+                <td><%=bean.getApellido() %></td>
+                <td><%=bean.getPassword() %></td>
+                <td><%=bean.getLogin() %></td>
+                <td><a href = "<%=context%>/UsuarioServlet?operacion=EliminarUsuario&UsuarioId=<%=bean.getIdUsuario() %>">ELIMINAR</a></td>
+                <td><a href = "<%=context%>/UsuarioServlet?operacion=modificarUsuario&UsuarioId=<%=bean.getIdUsuario() %>">MODIFICAR</a></td>
+                
+                
+            </tr>
+            <%
+            }
+            %>
+        </table>
+         <%
+        }
+        %>
+      
+
+    </body>
+</html>
